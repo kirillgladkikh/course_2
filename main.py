16eee9613a9f5acdcdb89a0b2a4a4a83bc0d15c1
Kirill Gladkikh <k.gladkikh@mail.ru>
2025-12-16 17:39:27 +0300

    main.py + utils.py: print_vacancy_count done wo tests

--- main.py
+++ main.py
@@ -122,8 +122,7 @@ def user_interaction():
     # - сохраняем "старое"+"новое" в all_vacancies
     all_vacancies = saver.add_vacancies(vacancies_for_json)
 
-    # - записываем all_vacancies в JSON-файл (предварительно преобразуя объекты Vacancy в словари!)
-    saver.save_vacancies_to_json(all_vacancies)
+    saver.save_vacancies_to_json(all_vacancies)  # Записываем all_vacancies в JSON-файл (предварительно преобразуя объекты Vacancy в словари!)
 
     # ФИЛЬТРУЕМ, СОРТИРУЕМ, ВЫВОДИМ ТОП ВАКАНСИЙ
 
@@ -142,88 +141,3 @@ def user_interaction():
 
 if __name__ == "__main__":
     user_interaction()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Преобразование набора данных из JSON в список объектов
-# vacancies_list = JSONSaver.write_vacancies(hh_vacancies)
-# print(vacancies_list)
-
-# # Пример работы контструктора класса с одной вакансией
-# vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-#
-# # Сохранение информации о вакансиях в файл
-# # Создаём экземпляр JSONSaver
-# json_saver = JSONSaver()
-# # Получаем список объектов Vacancy
-# vacancies = json_saver.get_vacancies()
-# # Выводим каждую вакансию
-# for vacancy in vacancies:
-#     print(f"Название: {vacancy.title}")
-#     print(f"Ссылка: {vacancy.url}")
-#     print(f"Зарплата: {vacancy.salary}")
-#     print(f"Требования: {vacancy.requirements}")
-#     print("-" * 40)  # разделитель между вакансиями
-# #
-
-
-# json_saver = JSONSaver()  # создаём объект
-# json_saver.write_vacancies(hh_vacancies)  # вызываем метод у объекта
-# print(json_saver.write_vacancies(hh_vacancies) )
-
-
-# json_saver = JSONSaver()
-# json_saver.add_vacancy(vacancy)
-# json_saver.delete_vacancy(vacancy)
-#
-# # Функция для взаимодействия с пользователем
-# def user_interaction():
-#     platforms = ["HeadHunter"]
-#     search_query = input("Введите поисковый запрос: ")
-#     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-#     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-#     salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
-#
-#     filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
-#
-#     ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
-#
-#     sorted_vacancies = sort_vacancies(ranged_vacancies)
-#     top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-#     print_vacancies(top_vacancies)
-
-
-# if __name__ == "__main__":
-#     user_interaction()
-
-
-# print("1====================")
-# text = "python"
-# hh = HHApi()
-# vacs = hh.get_vacancies(text)
-# # print(vacs)
-#
-# print("2====================")
-# js = JSONSaver()
-# js.write_vacancies(vacs)
-#
-# print("3====================")
-# vacancies = js.get_vacancies()
-# print_vacancies(vacancies)
